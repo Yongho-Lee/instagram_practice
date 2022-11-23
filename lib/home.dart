a190810d208011957b8331ac81897ac1c239ab76
Yongho-Lee <ylee197@myseneca.ca>
2022-11-22 22:25:29 -0500

    home.dart 쓸모 없어짐.. 이렇게 분산했더니 상속이 안됨. 상속해야 하는 법을 찾아야 할듯.

--- lib/home.dart
+++ lib/home.dart
@@ -1,27 +1,39 @@
 //home .dart
 import 'package:flutter/material.dart';
-
-var contents = ListView.builder(
-    itemCount: 3,
-    itemBuilder: (context, i){
-  return ListTile(
-    title: Image.network('https://codingapple1.github.io/app/car0.png'),
-
-    // Image.asset(
-    //     height: 50,
-    //     width: 50,
-    //     'water.jpg'),
-    subtitle: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-      Text('Home - subtitle Text1'),
-      Text('Home - subtitle Text2'),
-      Text('Home - subtitle Text3'),
-    ]),
-  );
-});
-
-//Text('Home 사진');
-
-
-
+//
+// class Home extends StatefulWidget {
+//   const Home({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//         itemCount: 3,
+//         itemBuilder: (context, i){
+//           return ListTile(
+//             title: Image.network('https://codingapple1.github.io/app/car0.png'),
+//
+//             // Image.asset(
+//             //     height: 50,
+//             //     width: 50,
+//             //     'water.jpg'),
+//             subtitle: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Text('Home - subtitle Text1'),
+//                   Text('Home - subtitle Text2'),
+//                   Text('Home - subtitle Text3'),
+//                 ]),
+//           );
+//         });
+//
+// //Text('Home 사진');
+//
+//
+//   }
+// }
+//
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+
+
 import './style.dart' as style;
 import './shop.dart' as shopTab;
 import './home.dart' as homeTab;
@@ -15,48 +17,62 @@ void main() {
 }
 
 
-
+// **  MYAPP  **
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
+
+  // bottom tab selection variable
   var tab = 0;
 
+  var datas = [];
 
+  // Json Get 요청
   getData() async {
-    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
-    print(jsonDecode(result.body));
+    var pureData = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    print(jsonDecode(pureData.body));
+    var result = jsonDecode(pureData.body);
+    setState(() {
+      datas = result;
+    });
   }
 
-  @override // initstate는 Myapp이 로드될 때 한 번 실행됨.
+  @override
+  // initstate 함수는 Myapp이 로드될 때 한 번 실행되는 라이브러리 함수임.
   void initState() {
     super.initState();
     getData();
   }
 
 
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      // ** AppBar
       appBar: AppBar(
         title: Text('instagram'),
         actions: [Icon(Icons.add_box_outlined)],
       ),
-      body: mainItems(tabNum : tab),
+
+      // ** Body
+      body:       Container(
+          child: [Home(datas : datas), shopTab.contents][tab]
+      ),
+
+      // **Navigation Bar
       bottomNavigationBar: BottomNavigationBar(
         showUnselectedLabels: false,
         showSelectedLabels: false,
-
+        // this function changes the tab number ( [home, shop] [0,1 ...] ) by state.
         onTap: (i){
           setState(() {
             tab = i;
           });
         },
-
         items: [
           BottomNavigationBarItem(
               label : '홈',
@@ -74,7 +90,7 @@ class _MyAppState extends State<MyApp> {
           )
         ],
 
-      //
+      // 내가 손으로 친 코드 (뭐가 문제인지 잘 모르겠음. 나중에 체크좀)
       //   items: [
       //   BottomNavigationBarItem(
       //     icon:IconButton(onPressed: (){},
@@ -109,29 +125,61 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+/// mainitems stful은 지움.
+/// getData 이후에 파일이 계속 상속해서 문제가 발생함. 해결책을 찾지 못함.
 
-class mainItems extends StatefulWidget {
-  const mainItems({Key? key,
-    required this.tabNum
 
-  }) : super(key: key);
+// ** Home
 
-  final tabNum;
+class Home extends StatefulWidget {
+  const Home({Key? key,
+    required this.datas,
+  })
+      : super(key: key);
+
+  final datas;
 
   @override
-  State<mainItems> createState() => _mainItemsState();
+  State<Home> createState() => _HomeState();
 }
 
-class _mainItemsState extends State<mainItems> {
+class _HomeState extends State<Home> {
   @override
+
+
+
   Widget build(BuildContext context) {
+    final datas = widget.datas;
 
-    final tabNum = widget.tabNum;
+    if(datas.isNotEmpty){
+      return ListView.builder(
+          itemCount: datas.length,
+          itemBuilder: (context, i){
+            return ListTile(
+              title: Image.network(datas[i]['image']),
 
-    return
-      Container(
-          child: [homeTab.contents, shopTab.contents][tabNum]
-      );
+              // Image.asset(
+              //     height: 50,
+              //     width: 50,
+              //     'water.jpg'),
+              subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(datas[i]['content']),
+                    Text(datas[i]['date']),
+                    Text(datas[i]['user']),
+                  ]),
+            );
+          });
+
+    } else {
+      return CircularProgressIndicator();
+    }
+
+//Text('Home 사진');
+
 
   }
 }
+
+
